aaa947e145f1621ad1a8d94b6923bc0759616f87
PetrPetr <ferel2015@yandex.ru>
2021-10-23 15:48:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Universal_massiv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArray = {1, 2, 3};
+            Resize<int>(ref myArray, 4);
+        }
+
+
+        static  void Resize<T>(ref int[] array, int newSize)
+        {
+           int[] newArray = new int[newSize];
+           for (int i = 0; i < array.Length && i < newArray.Length; i++)
+           {
+               newArray[i] = array[i];
+           }
+
+           array = newArray;
+        }
+    }
+}
